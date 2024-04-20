89dd284bad96138f0a2c74366ca34afb7792ba97
kbshin <amazing1997@naver.com>
2024-04-20 15:22:44 +0900

    Initial Commit

---
+++ HBDBro/HBDBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HBDBroApp.swift
+//  HBDBro
+//
+//  Created by KIBEOM SHIN on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HBDBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
